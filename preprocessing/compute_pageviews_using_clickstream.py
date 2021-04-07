785cc386eed4c1f34c2a96de39ccc675e47b4986
Tiffany <tifding@gmail.com>
2021-04-07 15:05:01 -0400

    convert to array

--- preprocessing/compute_pageviews_using_clickstream.py
+++ preprocessing/compute_pageviews_using_clickstream.py
@@ -14,7 +14,10 @@ folder = 'data/clickstream'
 title_to_idx_path = 'data/wikilinkgraph/title_to_idx_2018.pkl'
 
 # Location to save pageviews csv to
-save_to = 'data/pageviews.csv'
+save_csv_to = 'data/pageviews.csv'
+
+# Location to save pageviews np array to
+save_array_to = 'data/pageviews.npy'
 
 # -------------------------------
 
@@ -23,48 +26,58 @@ with open(title_to_idx_path, 'rb') as f:
     title_to_idx = pickle.load(f)
 print('title_to_idx keys sample:', list(title_to_idx.keys())[:20])
 
-# Get list of clickstream file names 
-files = []
-_, _, files_in_dir = next(os.walk(folder))
-for f in files_in_dir:
-    if f.endswith('tsv.gz'):
-        files.append(f)
-files = sorted(files)
+# # Get list of clickstream file names 
+# files = []
+# _, _, files_in_dir = next(os.walk(folder))
+# for f in files_in_dir:
+#     if f.endswith('tsv.gz'):
+#         files.append(f)
+# files = sorted(files)
 
-counts = pd.DataFrame()
-for zip_file in files:
-    ## Can uncomment to test
-    # df = pd.DataFrame({'curr': ['Anglo-Saxons', 'Neper', 'Alabama', 'Anglo-Saxons', 'bjrwgbbj'],
-    #                     'n': [10, 20, 30, 100, 200]})
+# counts = pd.DataFrame()
+# for zip_file in files:
+#     ## Can uncomment to test
+#     # df = pd.DataFrame({'curr': ['Anglo-Saxons', 'Neper', 'Alabama', 'Anglo-Saxons', 'bjrwgbbj'],
+#     #                     'n': [10, 20, 30, 100, 200]})
 
-    # Read in files
-    print('File: ', zip_file)
-    df = pd.read_csv(os.path.join(folder, zip_file), compression='gzip', sep='\t',
-                # nrows=1000, # can uncomment to test
-                names=['prev', 'curr', 'type', 'n'],
-                usecols=['curr', 'n'])
+#     # Read in files
+#     print('File: ', zip_file)
+#     df = pd.read_csv(os.path.join(folder, zip_file), compression='gzip', sep='\t',
+#                 # nrows=1000, # can uncomment to test
+#                 names=['prev', 'curr', 'type', 'n'],
+#                 usecols=['curr', 'n'])
 
-    num_rows = len(df)
-    print('Number of rows:', num_rows)
+#     num_rows = len(df)
+#     print('Number of rows:', num_rows)
 
-    ct = df.groupby('curr').sum()
-    counts = counts.append(pd.DataFrame(ct).reset_index())
+#     ct = df.groupby('curr').sum()
+#     counts = counts.append(pd.DataFrame(ct).reset_index())
 
-# Map page titles to index. Pages that did not appear in WikiLinkGraph
-# are mapped to -1 and then removed
-counts['page_idx'] = counts['curr'].apply(
-                lambda x: title_to_idx.get(str(x).replace('_', ' '), -1))
+# # Map page titles to index. Pages that did not appear in WikiLinkGraph
+# # are mapped to -1 and then removed
+# counts['page_idx'] = counts['curr'].apply(
+#                 lambda x: title_to_idx.get(str(x).replace('_', ' '), -1))
 
-final_counts = pd.DataFrame(counts.groupby('page_idx').sum()).reset_index()
-# Remove counts for pages that did not appear in WikiLinkGraph
-final_counts = final_counts[final_counts['page_idx']!=-1]
+# final_counts = pd.DataFrame(counts.groupby('page_idx').sum()).reset_index()
+# # Remove counts for pages that did not appear in WikiLinkGraph
+# final_counts = final_counts[final_counts['page_idx']!=-1]
 
 
-print(final_counts)
+# print(final_counts)
 
-# Save
-final_counts.to_csv(save_to)
-print(f'Saved pageviews csv to {save_to}')
+# # Save csv
+# final_counts.to_csv(save_csv_to)
+# print(f'Saved pageviews csv to {save_csv_to}')
+
+final_counts = pd.read_csv(save_csv_to)
+
+# Convert pageviews csv to np array and save
+pageviews_arr = np.zeros((len(title_to_idx), 1))
+for _, row in final_counts.iterrows():
+    pageviews_arr[row['curr']] = row['n']
+# Check number of pages with 0 views
+print(f'{np.sum(pageviews_arr == 0)} out of {len(pageviews_arr)} pages have 0 views')
+np.save(pageviews_arr, save_array_to)
 
 
 print(f'Time taken: {(time.time() - st) / 60:.2f} min')
