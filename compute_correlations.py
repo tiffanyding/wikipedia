04659df39be0a7fa3d7f65f3e07dfcd9563595cb
Tiffany <tifding@gmail.com>
2021-04-07 19:50:50 -0400

    transpose PR

--- compute_correlations.py
+++ compute_correlations.py
@@ -21,6 +21,8 @@ year = '2018'
 pageranks = load_pickle_file(f'results/pagerank_{year}.pkl')
 pageviews = np.load(f'results/pageviews_{year}.npy')
 
+pageranks = pageranks.T
+
 print('pageranks', pageranks.shape)
 print('pageviews', pageviews.shape)
 
