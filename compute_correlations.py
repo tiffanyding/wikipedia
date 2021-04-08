edcb3407903e5137aabaad9b2553100962bc93bc
Tiffany <tifding@gmail.com>
2021-04-07 23:12:51 -0400

    added rw2

--- compute_correlations.py
+++ compute_correlations.py
@@ -16,20 +16,24 @@ def load_pickle_file(path):
 
 def compute_corrcoeff(arr1, arr2):
     X = np.stack((np.squeeze(arr1), np.squeeze(arr2)), axis=1).T
-    print('X', X.shape)
     return np.corrcoef(X)[0][1]
 
 year = '2018'
-pageranks = load_pickle_file(f'results/pagerank_{year}.pkl')
 pageviews = np.load(f'results/pageviews_{year}.npy')
+
+pageranks = load_pickle_file(f'results/pagerank_{year}.pkl')
+rw2 = load_pickle_file(f'results/pagerank_{year}.pkl')
 
 pageranks = np.array(pageranks).T
 
-print('pageranks', pageranks.shape)
-print('pageviews', pageviews.shape)
+# print('pageranks', pageranks.shape)
+# print('pageviews', pageviews.shape)
 
-corr1 = compute_corrcoeff(pageranks, pageviews)
+corr1 = compute_corrcoeff(pageviews, pageranks)
 print(f'Correlation between page views and PageRank: {corr1:.3f}')
+
+corr2 = compute_corrcoeff(pageviews, rw2)
+print(f'Correlation between Model 2 and PageRank: {corr2:.3f}')
 
 
 
