af943bc859ea5fb67d2919033d460c19d7367e11
Tiffany <tifding@gmail.com>
2021-04-07 23:18:57 -0400

    added rw2

--- compute_correlations.py
+++ compute_correlations.py
@@ -20,6 +20,7 @@ def compute_corrcoeff(arr1, arr2):
     if arr2.shape[1] != 1:
         arr2 = arr2.T
 
+    print(arr1.shape, arr2.shape)
     X = np.stack((np.squeeze(arr1), np.squeeze(arr2)), axis=1).T
     return np.corrcoef(X)[0][1]
 
