40a5f5b9c70895a19d3e6a2e4aebb33a1a3e9018
Tiffany Ding <tifding@gmail.com>
2021-06-18 11:36:22 -0400

    added external vs. interval page view analysis

--- submit_job.sh
+++ submit_job.sh
@@ -4,5 +4,10 @@
 # For big memory jobs, use `qsub -l day -cwd -l vf=16G submit_job.sh`
 
 source env/bin/activate
-echo 'Running preprocessing/process_wikilinkgraph.py'
-python preprocessing/process_wikilinkgraph.py
+
+# echo 'Running preprocessing/process_wikilinkgraph.py'
+# python preprocessing/process_wikilinkgraph.py
+
+echo 'Running preprocessing/create_idx_to_topic_map.py'
+python preprocessing/create_idx_to_topic_map.py
+
